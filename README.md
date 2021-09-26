edaf46f8ae2648fb60b68046eb98d5607ef2eddf
Anuta Iuga <anuta.turlas@yahoo.com>
2021-09-26 16:05:11 +0300

    application_done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# meteo_app
+## meteo_app
+
+Application created with JS, HTML and CSS. 
+I used http://api.weatherapi.com/ in order to fetch real time weather data.
+Basically I am fetching data based on the user input.
+
+## Screenshots
+
+![screenshot](screenshots\meteo_app_screenshot.PNG)
--- meteo.js
+++ meteo.js
@@ -3,24 +3,43 @@ const city = document.querySelector("#search-box");
 const baseUrl = "http://api.weatherapi.com/v1/current.json"
 const key = "f27f5e73e6fe421ab82121715212409"
 
-const button = document.querySelector("#search-button");
-button.addEventListener("click", () => {
+const searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", listenForClick)
+
+const input = document.querySelector("#search-box");
+input.addEventListener("keypress", listenForKeyPress)
+
+function listenForKeyPress(event) {
+    if (event.key == "Enter") {
+        triggerFetchRequest();
+    }
+}
+ 
+function listenForClick() {
+    triggerFetchRequest();
+}
+ 
+function triggerFetchRequest() {
     const query = "?key=" + key + "&q=" + city.value;
     const link = baseUrl + query;
     fetch(link)
         .then(response => {
             return response.json();
-        }).then(result)
-})
+        }).then(fetchWeather)
+}
 
-function result(response) {
+function fetchWeather(response) {
     console.log(response)
     const loc = document.querySelector(".city");
     loc.innerText = response.location.name + ", " + response.location.region + ", " + response.location.country;
     const temp = document.querySelector(".temperature");
-    temp.innerHTML =  response.current.temp_c + "&#8451;"
+    temp.innerHTML = "Temperature: " + response.current.temp_c + "&#8451;"
     const icon = document.querySelector(".icon");
     icon.src = "http:" + response.current.condition.icon;
-    console.log(icon.src)
+    const hum = document.querySelector(".humidity");
+    hum.innerText = "Humidity: " + response.current.humidity + "%";
+    const description = document.querySelector(".description");
+    description.innerText = response.current.condition.text;
+    const date = document.querySelector(".date");
+    date.innerText = response.location.localtime;
 }
-
